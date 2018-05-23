f66b7caba023818998aecc9d1f83d63d62607afd
borisaeric <borisa123@hotmail.com>
2018-05-23 11:50:55 +0200

    add migration create_standings_table

---
+++ database/migrations/2018_05_23_094756_create_standings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStandingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('standings', function (Blueprint $table) {
+            $table->integer('club_id')->unsigned()->nullable();
+            $table->foreign('club_id')->references('id')->on('clubs')->onDelete('cascade');
+            $table->integer('season_id')->unsigned()->nullable();
+            $table->foreign('season_id')->references('id')->on('seasons')->onDelete('cascade');
+            $table->integer('wins');
+            $table->integer('draws');
+            $table->integer('losses');
+            $table->integer('goals_scored');
+            $table->integer('goals_conceded');
+            $table->integer('points');   
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('standings');
+    }
+}
